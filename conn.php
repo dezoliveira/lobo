4d1835d4226776c217ce953922a3a0db97e1c9c4
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-12 00:03:50 -0300

    add: conn

---
+++ conn.php
@@ -0,0 +1,26 @@
+<?php
+  $server = 'localhost';
+  $user = 'root';
+  $password = '';
+  $db = 'lobo';
+
+  $conn = mysqli_connect(
+    hostname: $server,
+    username: $user,
+    password: $password,
+    database: $db
+  );
+
+  try {
+    if (!$conn){
+      throw new Exception('Unable to connect');
+      
+    } else {
+      echo "Connected";
+      header("Location:login.php");
+      exit;
+    }
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+?>
