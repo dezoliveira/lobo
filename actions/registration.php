27fcdc93d378aca978d4970224cb0405f52d62a0
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-13 00:08:27 -0300

    add: encode64 on password

--- actions/registration.php
+++ actions/registration.php
@@ -3,7 +3,7 @@
 require_once '../conn.php';
 
 $name = $_POST['name'];
-$password = $_POST['password'];
+$password = base64_encode( $_POST['password']);
 $email = $_POST['email'];
 $date = date('Y-m-d H:i:s');
 
@@ -15,20 +15,19 @@ $total_users = mysqli_num_rows($results);
 // if exists
 if ($total_users > 0) {
   echo "email already exists";
-
-} else {
-  $query = "INSERT INTO login (name, email, password, created_At) VALUES ('$name', '$email', '$password', '$date')";
-  $results = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  exit();
 
 }
+
+$query = "INSERT INTO login (name, email, password, created_At) VALUES ('$name', '$email', '$password', '$date')";
+$results = mysqli_query($conn, $query) or die(mysqli_error($conn));
 
 if (!$results) {
   echo "erro 500";
-
-} else {
-  echo'<script> window.location="../messages/register_success.php"; </script> ';
+  exit();
 
 }
 
+  echo'<script> window.location="../messages/register_success.php"; </script> ';
 
 ?>
